6fc07c0b7def4801ecaf00d9ec0c6c7bb4a68e7b
blue3eight <blue3eight@gmail.com>
2020-12-17 12:09:52 +0100

    Update test1.py
    - ajout possibilité de passer une image en paramètre
    - possibilité de travailler sur un seul layer

--- test1.py
+++ test1.py
@@ -1,31 +1,29 @@
 from PIL import Image 
 
-def revalLBSImage(imagePath,nbits=1):
-  
+def revalLBSImage(imgage_or_imagePath, nbits=1):
+    
     def getLBS(byte,n=1):
         byte = byte & 255>>8-n
         byte = byte << 8 - n
         return byte
-    
-    img = Image.open(imagePath)
+  
+    if isinstance(imgage_or_imagePath, str):
+        img = Image.open(imgage_or_imagePath)
+    else :
+        img = imgage_or_imagePath
+        
     width,heigh = img.size
-    for row in range(heigh):
-        for col in range(width):
-            (r,g,b) = img.getpixel((col,row))
-            r = getLBS(r,nbits)
-            g = getLBS(g,nbits)
-            b = getLBS(b,nbits)
-            img.putpixel((col,row),(r,g,b))
+    if img.mode == 'RGB' :
+        for row in range(heigh):
+            for col in range(width):
+                (r,g,b) = img.getpixel((col,row))
+                r = getLBS(r,nbits)
+                g = getLBS(g,nbits)
+                b = getLBS(b,nbits)
+                img.putpixel((col,row),(r,g,b))
+    else :
+        for row in range(heigh):
+            for col in range(width):
+                l = img.getpixel((col,row))
+                img.putpixel((col,row), getLBS(l,nbits))
     return img
-
- def split_by_n( seq, n ):
-   """A generator to divide a sequence into chunks of n units."""
-   while seq:
-       yield seq[:n]
-       seq = seq[n:]
-
-def triplexTobin(l):
-    txt=""
-    for a,b,c in l:
-        txt=txt+str(a)+str(b)+str(c)
-    return txt
